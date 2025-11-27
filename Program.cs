779f72e753264da21e4eab9f2397049eeff6dcdd
David Souza <sou17010@byui.edu>
2025-11-27 00:44:53 -0300

    add missing code

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using dotNetWedEve.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSingleton<Model.SimpleAuthService>();
+builder.Services.AddSqlite<dotNetWedEveContext>("Data Source=dotNetWedEve.db");
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
