d03bc1c8347e51356b8f66ff5ea577e659b7bc60
David Souza <sou17010@byui.edu>
2025-11-27 01:17:28 -0300

    adding transaction class

--- Model/Transaction.cs
+++ Model/Transaction.cs
@@ -1,10 +1,18 @@
 namespace dotNetWedEve
 {
+    public enum TransactionType
+    {
+        Expense,
+        Income
+    }
+
     public class Transaction
     {
         public required string Name { get; set; }
-        
-        public required decimal Amount {get; set; }
-
+        public required decimal Amount { get; set; }
+        public required Category Category { get; set; }
+        public required TransactionType Type { get; set; }
+        public DateTime Date { get; set; }
+        public string? Notes { get; set; }
     }
 }
