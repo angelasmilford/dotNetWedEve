cf6556c6d60c3922188c38317b2cc1b6c5d0248c
David Souza <sou17010@byui.edu>
2025-11-27 00:18:23 -0300

    adding classes initial

---
+++ Model/Transaction.cs
@@ -0,0 +1,10 @@
+namespace dotNetWedEve
+{
+    public class Transaction
+    {
+        public required string Name { get; set; }
+        
+        public required decimal Amount {get; set; }
+
+    }
+}
